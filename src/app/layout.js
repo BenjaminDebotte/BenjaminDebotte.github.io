839c1c00c0007fe52f2446166740dc332ffd4b2e
Benjamin Debotte <benjamin.debotte@gmail.com>
2023-10-31 15:39:41 +0100

    style: fixes color discrepencies by removing useless div

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,26 +41,24 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body>
+      <body className={clsx(
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        GeistSans.variable,
+        GeistMono.variable
+      )}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           disableTransitionOnChange
         >
-          <div className={clsx(
-            'text-black bg-white dark:text-white dark:bg-[#111010]',
-            GeistSans.variable,
-            GeistMono.variable
-          )}>
-            <div className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-              <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-                <Navbar />
-                {children}
-              </main>
-            </div>
+          <div className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
+            <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+              <Navbar />
+              {children}
+            </main>
           </div>
         </ThemeProvider>
-      </body>
+      </body >
     </html >
   );
 
